0bcdab8e3484a2ddae2b32c75ab24d41dcfafe2f
BalazsAntal <antal.balaazs@gmail.com>
2022-10-11 15:21:08 +0200

    initial code for session

---
+++ lesson2.R
@@ -0,0 +1,19 @@
+#Qs - loading data, source(), save workspace
+
+
+# R Objects ---------------------------------------------------------------
+
+
+#atomic vectors
+### one(times n)-dimensional vector
+### stores only one type of data
+a <- 5
+
+die <- 1:6
+
+is.vector(a)
+is.vector(die)
+
+length(a)
+length(die)
+
